015ee430e77bf06562c83e0864b73709bcba5899
Siddharth Rawat <siddharth@MacBook-Air.local>
2025-08-17 14:07:30 +1000

    Remove debug Home screen; redirect / to /login

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
 from livereload import Server
+from flask import redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def login():  # sourcery skip: remove-redundant-if
     if request.method == 'POST':
         # handle login logic here (e.g., authentication)
         pass
@@ -17,12 +18,16 @@ def bookshelf():
     return render_template('bookshelf.html')
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def register():  # sourcery skip: remove-redundant-if
     if request.method == 'POST':
         # handle registration logic here
         pass
     return render_template('register.html')
 
+def root():
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     server = Server(app.wsgi_app)
     server.serve(port=5001, debug=True)
+
